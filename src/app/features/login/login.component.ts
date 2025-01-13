4c1949a4808c664a47f9c4183b3568c981198a48
Erivando Castro <163551939+ErivandoCastro@users.noreply.github.com>
2025-01-13 16:45:41 -0300

    feat: criando login

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  identificacao: string = '';
+  senha: string = '';
+  passwordVisible: boolean = false;
 
+  // Alterna a visibilidade da senha
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  // Lida com o envio do formulário
+  onLogin(event: Event): void {
+    event.preventDefault(); // Previne o recarregamento da página
+    console.log('Identificação:', this.identificacao);
+    console.log('Senha:', this.senha);
+    // Adicione sua lógica de autenticação aqui
+  }
 }
+
+
+
+
+
+/**import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+})
+export class LoginComponent {
+  loginForm!: FormGroup
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+} **/
