8621721617402f5adb40d7615e10bf427bc918cd
Enderson <enderson.linhares20@gmail.com>
2025-02-08 13:37:42 -0300

    feat: criando a sessao de details da pagina de interact

--- src/app/features/victims/interact/interact.component.ts
+++ src/app/features/victims/interact/interact.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-interact',
-  imports: [],
+  imports: [FormsModule, MatIcon],
   templateUrl: './interact.component.html',
   styleUrl: './interact.component.scss'
 })
 export class InteractComponent {
+  isCustom: boolean = false;
+
+  toggleCustom() {
+    this.isCustom = !this.isCustom;
+  }
 
 }
