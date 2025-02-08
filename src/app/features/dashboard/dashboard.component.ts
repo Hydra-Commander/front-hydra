21eff8ca962e112b747445cc14a07cd0bdd3e824
Enderson <enderson.linhares20@gmail.com>
2025-02-08 12:28:24 -0300

    feat: componentizando o card-icon da dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
+import { CardIconComponent } from "../../shared/components/card-icon/card-icon.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatIcon],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  imports: [CardIconComponent]
 })
 export class DashboardComponent {
-
+  contents = [
+    {
+      title: 'Total Users',
+      counter: 5,
+      iconName: 'groups'
+    },
+    {
+      title: 'Active Users',
+      counter: 2,
+      iconName: 'record_voice_over'
+    },
+    {
+      title: 'Active Sessions',
+      counter: 2,
+      iconName: 'computer'
+    },
+    {
+      title: 'Critical & Important Log',
+      counter: 0,
+      iconName: 'task'
+    }
+  ]
 }
