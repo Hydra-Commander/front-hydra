809a6f7814c30cf1ef39f399e3e4f701e0219707
Enderson <enderson.linhares20@gmail.com>
2025-02-08 17:32:25 -0300

    feat: criando a section de command

--- src/app/features/victims/interact/interact.component.ts
+++ src/app/features/victims/interact/interact.component.ts
@@ -1,10 +1,11 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-interact',
-  imports: [FormsModule, MatIcon],
+  imports: [FormsModule, MatIcon, UpperCasePipe],
   templateUrl: './interact.component.html',
   styleUrl: './interact.component.scss'
 })
