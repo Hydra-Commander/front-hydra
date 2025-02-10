a8e4b63e352427bbd90cefbf29817b9ae8072950
Enderson <enderson.linhares20@gmail.com>
2025-02-10 20:25:00 -0300

    feat: integrando com o backend

--- src/app/features/victims/interact/interact.component.ts
+++ src/app/features/victims/interact/interact.component.ts
@@ -1,7 +1,11 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIcon } from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
+import { Victim } from '../../../shared/types/victims.interface';
+import { ApiService } from '../../../shared/services/api.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-interact',
@@ -9,8 +13,32 @@ import { MatIcon } from '@angular/material/icon';
   templateUrl: './interact.component.html',
   styleUrl: './interact.component.scss'
 })
-export class InteractComponent {
+export class InteractComponent implements OnInit {
   isCustom: boolean = false;
+  victimId!: number;
+  victim!: Victim;
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly apiService: ApiService
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.victimId = params['victimId'];
+    })
+
+    if(this.victimId){
+      this.apiService.getAgentById(this.victimId).pipe(take(1)).subscribe({
+        next: (data) => {
+          this.victim = data;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+    }
+
+  }
 
   toggleCustom() {
     this.isCustom = !this.isCustom;
